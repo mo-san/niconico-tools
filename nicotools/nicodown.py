8b17daacf67b4e64ddee3dce15b86bcd1609f7fa
mo-san <hi3nterland9>
2016-12-23 03:58:32 +0900

    utf-8 → UTF-8

--- nicotools/nicodown.py
+++ nicotools/nicodown.py
@@ -1,4 +1,4 @@
-# coding: utf-8
+# coding: UTF-8
 import html
 import os
 import socket
--- nicotools/stubs/nicodown.pyi
+++ nicotools/stubs/nicodown.pyi
@@ -1,4 +1,4 @@
-# coding: utf-8
+# coding: UTF-8
 import logging
 import requests
 from abc import ABCMeta, abstractmethod
