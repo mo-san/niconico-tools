36c5a1c228dc930c72f84a6cf1583a15e923cf19
mo-san <window100@gmail.com>
2018-09-14 18:58:30 +0900

    Travis用スクリプトの修正

--- tests/travis.sh
+++ tests/travis.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 suffix=`date "+%Y%m%d_%H%M"`_${TRAVIS_PYTHON_VERSION}
-log_nd=nicotools_download.log
-log_ml=nicotools_mylist.log
+log_file=nicotools.log
 
-curl -X PUT -H "Authorization: Bearer $DROPBOX_TOKEN" -T ~/${log_nd} https://content.dropboxapi.com/1/files_put/auto/Travis/${suffix}/${log_nd}
-curl -X PUT -H "Authorization: Bearer $DROPBOX_TOKEN" -T ~/${log_ml} https://content.dropboxapi.com/1/files_put/auto/Travis/${suffix}/${log_ml}
+curl -X POST https://content.dropboxapi.com/2/files/upload\
+  --header "Authorization: Bearer ${DROPBOX_TOKEN}"\
+  --header 'Dropbox-API-Arg: {"path": "/Travis/${suffix}_${log_file}", "mode": "add"}'\
+  --header "Content-Type: application/octet-stream"\
+  --data-binary @${log_file}
